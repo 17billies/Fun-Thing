de845f34a1e14fd6b9e962a0639c748133fdd45c
Kitty Chao <kittychao520@gmail.com>
2021-05-07 16:02:49 +0800

    functioning insertion sorting and completed code

--- blackhole/black_hole_game.c
+++ blackhole/black_hole_game.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<string.h>
+void insertion_sorting(int input[]);
 int input[5]={0};
 int trans[5]={0};
+int max,min,ans;
  main(){
     int in,ten;
     int i=0,j=0,t=0,temp;
@@ -17,31 +19,53 @@ int trans[5]={0};
                 printf("%d",input[t]);
             }
             printf("\n");*/
-        //insertion sorting
-        for(i=0;i<4;++i){
-            j=i;
-            while(j>0 && input[j-1]<input[j]){
-                temp=input[j-1];
-                input[j-1]=input[j];
-                input[j]=temp;
-                --j;
-            }
-           /* for(t=0;t<4;++t){
-                printf("%d\n",input[t]);
-            }*/
-        }
-
+        insertion_sorting(input);
         //calculate the max and min
-        int max,min,ans;
-        max=input[0]*1000+input[1]*100+input[2]*10+input[3];
-        for(i=3;i>=0;--i){
-            trans[3-i]=input[i];
-        }
-        min=trans[0]*1000+trans[1]*100+trans[2]*10+trans[3];
-        ans=max-min;
+
+
         printf("max=%d\n",max);
         printf("min=%d\n",min);
         printf("ans=%d\n",ans);
+
+        int former=0;
+        int num[5];
+
+        while(former!=ans){
+            former=ans;
+            ten=1000;
+            for(i=0;i<4;++i){
+                num[i]=ans/ten;
+                ans=ans%ten;
+                ten=ten/10;
+            }
+            insertion_sorting(num);
+            printf("max=%d\n",max);
+            printf("min=%d\n",min);
+            printf("ans=%d\n",ans);
+        }
+
     }
     return 0;
  }
+void insertion_sorting(int input[]){
+    int i=0,j=0;
+    int temp;
+    for(i=0;i<4;++i){
+        j=i;
+        while(j>0 && input[j-1]<input[j]){
+            temp=input[j-1];
+            input[j-1]=input[j];
+            input[j]=temp;
+            --j;
+        }
+           /* for(t=0;t<4;++t){
+                printf("%d\n",input[t]);
+            }*/
+    }
+    max=input[0]*1000+input[1]*100+input[2]*10+input[3];
+    for(i=3;i>=0;--i){
+        trans[3-i]=input[i];
+    }
+    min=trans[0]*1000+trans[1]*100+trans[2]*10+trans[3];
+    ans=max-min;
+}
